fb4152cc1d8564fba58a660e15af5c3c4c25b653
rohithpeddi7 <peddi.rohith.cse21@itbhu.ac.in>
2022-12-29 15:56:30 +0530

    added search in google

--- ok.py
+++ ok.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from textwrap3 import wrap
 from AppOpener import run
+import pywhatkit as pwt
 
 from io import StringIO # Python3 use: from io import StringIO
 import sys
@@ -39,6 +40,7 @@ def gpt(query):
 def all_commands():
 	cmds = {"-h or help":"Open manual",
 			"<no option> query": "Use gpt-3 to answer your query",
+			"-s <query>":"Searches query in google",
 			"-o <AppName>":"Open Application",
 			"-o ls":"List All Applications",
 			"-o find <AppName>":"Find Application",
@@ -62,11 +64,14 @@ def send():
 			sys.stdout = mystdout = StringIO()
 			old_stdout = sys.stdout
 			run(app)
-			answer = mystdout.getvalue()
+			answer = mystdout.getvalue().capitalize()
 		else:
 			try:
 				if str(user)=="-h" or str(user)=="help":
 					answer=all_commands()
+				elif user[0:2]=="-s":
+					pwt.search(user[2:].strip())
+					answer="Redirecting to Google.."
 				else:
 					answer = gpt(user)
 			except Exception as er:
@@ -82,13 +87,13 @@ def send():
 lable1 = Label(root, bg=BG_COLOR, fg=TEXT_COLOR, text="AI assistant", font=FONT_BOLD, pady=10, width=20, height=1).grid(
 	row=0)
 
-txt = Text(root, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
+txt = Text(root, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60,wrap=WORD)
 txt.grid(row=1, column=0, columnspan=2)
 
-scrollbar = Scrollbar(txt)
+scrollbar = Scrollbar(txt,borderwidth=1)
 scrollbar.place(relheight=1, relx=0.974)
 
-e = Entry(root, bg="#2C3E50", fg=TEXT_COLOR, font=FONT, width=50)
+e = Entry(root, bg="#2C3E50", fg=TEXT_COLOR, font=FONT, width=55)
 e.grid(row=2, column=0)
 
 send = Button(root, text="Send", font=FONT_BOLD, bg=BG_GRAY,
