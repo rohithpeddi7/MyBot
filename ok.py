c27f86491254f2e25af0859f5ab784bfafacff77
rohithpeddi7 <peddi.rohith.cse21@itbhu.ac.in>
2022-12-29 15:00:51 +0530

    added menu

--- ok.py
+++ ok.py
@@ -5,14 +5,6 @@ from AppOpener import run
 from io import StringIO # Python3 use: from io import StringIO
 import sys
 
-old_stdout = sys.stdout
-
-# blah blah lots of code ...
-
-
-
-# examine mystdout.getvalue()
-
 # GUI
 root = Tk()
 root.title("MyBot")
@@ -27,7 +19,8 @@ FONT_BOLD = "Helvetica 13 bold"
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = "sk-LfURf8m7mqGQZ9PIR1Y2T3BlbkFJD4poErFSUfCvn4EhV"# add X2m at end
+os.environ["OPENAI_API_KEY"] = openai.api_key
 # openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def gpt(query):
@@ -43,11 +36,26 @@ def gpt(query):
 	)
 	return response["choices"][0]["text"].strip()
 
+def all_commands():
+	cmds = {"-h or help":"Open manual",
+			"<no option> query": "Use gpt-3 to answer your query",
+			"-o <AppName>":"Open Application",
+			"-o ls":"List All Applications",
+			"-o find <AppName>":"Find Application",
+			"-o update -m <AppName>":"Update Application Manually",
+			}
+	count=0
+	ans="The following are the options::\n"
+	for cmd,func in cmds.items():
+		count+=1
+		ans+=str(count)+". "+cmd+" : "+func+"\n"
+	return ans
+
 # Send function
 def send():
 	send_this = "You : " + e.get()
 	txt.insert(END, "\n" + send_this)
-	user = e.get().lower()
+	user = e.get().lower().strip()
 	try:
 		if user[0:2]=="-o":
 			app = user[2:].strip()
@@ -56,9 +64,17 @@ def send():
 			run(app)
 			answer = mystdout.getvalue()
 		else:
-			answer = gpt(user)
-	except:
-		answer="Enter valid input!"
+			try:
+				if str(user)=="-h" or str(user)=="help":
+					answer=all_commands()
+				else:
+					answer = gpt(user)
+			except Exception as er:
+				answer =gpt(user)
+	except Exception as er:
+		print(user)
+		print(type(er).__name__)
+		answer="Enter valid input! or report an error."
 	txt.insert(END, "\n" + "Bot : "+answer)
 	e.delete(0, END)
 
@@ -77,5 +93,7 @@ e.grid(row=2, column=0)
 
 send = Button(root, text="Send", font=FONT_BOLD, bg=BG_GRAY,
 			command=send).grid(row=2, column=1)
+
+txt.insert(END, "\n" + "Welcome to the next generation AI assistant!\nCreated by Rohith Peddi and Gaurav Mahendraker.\nAll rights reserved.\n\nPlease enter your query/command below.\nEnter help or -h for list of options.\n")
 
 root.mainloop()
